26896123a281834a21d2495f347b8d51ff364251
PradyLinux <pradyumna.p.6969@gmail.com>
2024-11-20 12:28:47 +0530

    Removed: redudnant code which called dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
 import taskRoutes from "./routes/taskRouter";
 import { errorHandler } from "./middlewares/errorHandler";
 import kleur from "kleur";
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
