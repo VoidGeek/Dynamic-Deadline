1bc0aa8065c369c82a4370d96d0bb73e6d182fd6
PradyLinux <pradyumna.p.6969@gmail.com>
2024-11-20 23:03:29 +0530

    Added: custom fields

--- src/interfaces/task.ts
+++ src/interfaces/task.ts
@@ -1,7 +1,9 @@
-// Task model interface
+// interfaces/task.ts
+
 export interface Task {
   gid: string;
   name: string;
   due_on?: string; // Optional since it might be undefined
   priority?: string;
+  custom_fields?: { [key: string]: any }; // Custom fields are key-value pairs
 }
